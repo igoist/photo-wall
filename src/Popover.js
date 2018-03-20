cbe5328e4626736161e83c708b541dda92889d7e
igoist <493512132@qq.com>
2018-03-20 20:19:20 +0800

    dev: control the img size, position, and use the chrome default way 'overflow: auto' to handle scrollbar

--- src/Popover.js
+++ src/Popover.js
@@ -6,7 +6,32 @@ class Popover extends React.Component {
   }
 
   componentDidMount() {
-    console.log('Popover created.');
+    let img = this.img;
+    let iw = img.width;
+    let ih = img.height;
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    {/* console.log('width: ', iw); */}
+    {/* console.log('height: ', ih); */}
+    {/* console.log('w width: ', w); */}
+    {/* console.log('w height: ', h); */}
+
+    let x = (w - iw) / 2;
+    let y = (h - ih) / 2;
+    let scaleX = 1;
+    let scaleY = 1;
+    if (iw < w) {
+      if (ih > h) {
+        y = 0;
+      }
+      {/* } else if (iw > w && ih < h) { */}
+    } else {
+      scaleY = scaleX = w / iw;
+      {/* scaleY = scaleX = iw / w > ih / h ? w / iw : h / ih; */}
+    }
+    let tmpStyle = `transform: translate3d(${x}px, ${y}px, 0px) scale3d(${scaleX}, ${scaleY}, 1);`;
+    img.style = tmpStyle;
+
     setTimeout(() => {
       this.wrap.classList.remove('fade-in');
     }, 20);
@@ -14,14 +39,15 @@ class Popover extends React.Component {
 
   componentWillUnmount() {
     this.wrap.classList.add('fade-out');
-    console.log('Clear');
   }
 
   render() {
     const { handleClick, src } = this.props;
     return (
       <div className='popover fade-in' ref={ (wrap) => { this.wrap = wrap; }} onClick={ handleClick }>
-        <img src={ src } />
+        <div className='popover-inner'>
+          <img src={ src } ref={ (img) => { this.img = img; }} style={{}} />
+        </div>
       </div>
     );
   }
